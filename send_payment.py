78b5758626b399772fdc561c7b8e3463ec3243cd
theorasparrow <152971598+theorasparrow@users.noreply.github.com>
2023-12-05 12:07:06 +0100

    Create send_payment.py

---
+++ send_payment.py
@@ -0,0 +1,12 @@
+# account of grantor
+grantor = "grantor"
+# account of prover
+prover = "prover"
+# account of maintainer
+maintainer = "maintainer"
+
+bounty = 100
+commission = bounty / 10
+
+print("transfering", bounty, "zoots", "from", grantor, "to", prover)
+print("transfering", commission, "zoots", "from", grantor, "to", maintainer)
