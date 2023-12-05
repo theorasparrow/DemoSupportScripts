57fa30221a0bf572e2213ca4c7614bd9c7dd6189
theorasparrow <152971598+theorasparrow@users.noreply.github.com>
2023-12-05 11:47:17 +0100

    Update check_theorem_exists.py

--- check_theorem_exists.py
+++ check_theorem_exists.py
@@ -1,1 +1,11 @@
-
+# assumes file name main.lean
+# assumes theorem `theorem test : 1 + 1 = 2 :=`
+flag = False
+with open('main.lean') as mainlean:
+  for ln in mainlean:
+    if ln.startswith("theorem test : 1 + 1 = 2 :="):
+      print("theorem statement exists")
+      flag = True
+      break
+if flag is False:
+  print(1/0)
