8c7ac4453852032779d67c1e4c8f2a6e3c09abea
theorasparrow <152971598+theorasparrow@users.noreply.github.com>
2023-12-07 14:01:40 +0100

    Update check_theorem_exists.py

--- check_theorem_exists.py
+++ check_theorem_exists.py
@@ -1,3 +1,16 @@
+import os
+
+# get commented open theorem from main branch
+# raw files in main branch
+root = "https://raw.githubusercontent.com/theorasparrow/LeanDemo/main/"
+currentmainpath = 'src/main.lean'
+currentoldfile = root + currentpath
+
+with open(currentoldfile) as currentoldlean:
+  for ln in currentoldlean:
+    if ln.startswith("-- theorem"):
+      print("commented open theorem from main branch: ", ln[2:])
+
 # assumes file name main.lean
 # assumes theorem `theorem test : 1 + 1 = 2 :=`
 flag = False
@@ -9,3 +22,21 @@ with open('src/main.lean') as mainlean:
       break
 if flag is False:
   print(1/0)
+
+
+
+
+
+# for path, subdir, files in os.walk('src'):
+#   for name in files:
+#     # paths of files
+#     currentpath = os.path.join(path, name)
+#     print(currentpath)
+#     with open(currentpath) as currentlean:
+#       for ln in currentlean:
+#         if ln.startswith("theorem test2 : 2 + 2 = 4 :="):
+#           print("open theorem exists")
+#           flag = True
+#           break
+#     if flag is False:
+#       print(1/0)
