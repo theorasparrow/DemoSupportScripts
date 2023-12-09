7c2648c0508ed7451b2a52effe50239dc926cbda
theorasparrow <152971598+theorasparrow@users.noreply.github.com>
2023-12-09 13:26:21 +0100

    Update send_payment.py

--- send_payment.py
+++ send_payment.py
@@ -1,12 +1,43 @@
+import sys
+from web3 import Web3, EthereumTesterProvider
+
+w3 = Web3(EthereumTesterProvider())
+
+account_prover = sys.argv[1].split("Eth")[1].split(" ")[0]
+account_grantor = w3.eth.accounts[0]
+account_maintainer = w3.eth.accounts[1]
+
+print("account prover", account_prover)
+print("account grantor", account_grantor)
+print("account maintainer", account_maintainer)
+
+tx1_hash = w3.eth.send_transaction({
+  "from" : account_grantor,
+  "to" : account_prover,
+  "value" : 100
+})
+
+tx2_hash = w3.eth.send_transaction({
+  "from" : account_grantor,
+  "to" : account_maintainer,
+  "value" : 10
+})
+
+tx1 = w3.eth.get_transaction(tx1_hash)
+tx2 = w3.eth.get_transaction(tx2_hash)
+
+print("tx1", tx1)
+print("tx2", tx2)
+
 # account of grantor
-grantor = "grantor"
+# grantor = "grantor"
 # account of prover
-prover = "prover"
+# prover = "prover"
 # account of maintainer
-maintainer = "maintainer"
+# maintainer = "maintainer"
 
-bounty = 100
-commission = bounty / 10
+# bounty = 100
+# commission = bounty / 10
 
-print("transfering", bounty, "zoots", "from", grantor, "to", prover)
-print("transfering", commission, "zoots", "from", grantor, "to", maintainer)
+# print("transfering", bounty, "zoots", "from", grantor, "to", prover)
+# print("transfering", commission, "zoots", "from", grantor, "to", maintainer)
