bb763e2ec95de98f36aa178f78a4c1c4d482c6ea
theorasparrow <152971598+theorasparrow@users.noreply.github.com>
2023-12-12 10:45:48 +0100

    Update send_payment.py

--- send_payment.py
+++ send_payment.py
@@ -1,31 +1,38 @@
 import sys
-from web3 import Web3, EthereumTesterProvider
+from web3 import Web3, EthereumTesterProvider, HTTPProvider
 
-w3 = Web3(EthereumTesterProvider())
+w3_test = Web3(EthereumTesterProvider())
+
+w3 = Web3(HTTPProvider('https://eth.llamarpc.com'))
+print("w3 connected?", w3.is_connected())
+print("w3 latest block", w3.get_block('latest'))
+account_grantor = w3.personal.newAccount('pass_grantor')
+print("account grantor", account_grantor)
+
 
 account_prover = sys.argv[1].split("Eth ")[1]
-# account_prover = w3.eth.accounts[2]
-account_grantor = w3.eth.accounts[0]
-account_maintainer = w3.eth.accounts[1]
+# account_prover = w3_test.eth.accounts[2]
+account_grantor_test = w3_test.eth.accounts[0]
+account_maintainer_test = w3_test.eth.accounts[1]
 
 print("account prover", account_prover)
-print("account grantor", account_grantor)
-print("account maintainer", account_maintainer)
+print("account grantor test", account_grantor_test)
+print("account maintainer test", account_maintainer_test)
 
-tx1_hash = w3.eth.send_transaction({
-  "from" : account_grantor,
+tx1_hash = w3_test.eth.send_transaction({
+  "from" : account_grantor_test,
   "to" : account_prover,
   "value" : 100
 })
 
-tx2_hash = w3.eth.send_transaction({
-  "from" : account_grantor,
-  "to" : account_maintainer,
+tx2_hash = w3_test.eth.send_transaction({
+  "from" : account_grantor_test,
+  "to" : account_maintainer_test,
   "value" : 10
 })
 
-tx1 = w3.eth.get_transaction(tx1_hash)
-tx2 = w3.eth.get_transaction(tx2_hash)
+tx1 = w3_test.eth.get_transaction(tx1_hash)
+tx2 = w3_test.eth.get_transaction(tx2_hash)
 
 print("tx1", tx1)
 print("tx2", tx2)
