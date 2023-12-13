63bc5eced88b694c71dd12508096b01321be11fc
theorasparrow <152971598+theorasparrow@users.noreply.github.com>
2023-12-13 14:27:56 +0100

    Update send_payment.py

--- send_payment.py
+++ send_payment.py
@@ -1,13 +1,14 @@
+import os
 import sys
 from web3 import Web3, EthereumTesterProvider, HTTPProvider
+from web3.middleware import construct_sign_and_send_raw_middleware
+
 
 w3_test = Web3(EthereumTesterProvider())
-
 # w3 = Web3(HTTPProvider('https://eth.llamarpc.com'))
+
 # print("w3 connected?", w3.is_connected())
 # print("w3 latest block", w3.eth.get_block('latest'))
-# account_grantor = w3.eth.personal.newAccount('pass_grantor')
-# print("account grantor", account_grantor)
 
 # 0x6813Eb9362372EEF6200f3b1dbC3f819671cBA69
 account_prover = sys.argv[1].split("Eth ")[1]
@@ -36,8 +37,8 @@ tx2_hash = w3_test.eth.send_transaction({
 tx1 = w3_test.eth.get_transaction(tx1_hash)
 tx2 = w3_test.eth.get_transaction(tx2_hash)
 
-print("tx1", tx1)
-print("tx2", tx2)
+# print("tx1", tx1)
+# print("tx2", tx2)
 
 # account of grantor
 # grantor = "grantor"
@@ -49,5 +50,41 @@ print("tx2", tx2)
 # bounty = 100
 # commission = bounty / 10
 
-# print("transfering", bounty, "zoots", "from", grantor, "to", prover)
-# print("transfering", commission, "zoots", "from", grantor, "to", maintainer)
+print("transfering", bounty, "zoots", "from", grantor, "to", prover)
+print("transfering", commission, "zoots", "from", grantor, "to", maintainer)
+
+
+
+grantor_pk = os.environ.get('GRANTOR_PK')
+maintainer_pk = os.environ.get('MAINTAINER_PK')
+
+# Instantiate Account object from key:
+grantor_acc = w3.eth.account.from_key(grantor_pk)
+maintainer_acc = w3.eth.account.from_key(maintainer_pk)
+
+# prover_address = 
+
+# auto-signer:
+w3.middleware_onion.add(construct_sign_and_send_raw_middleware(grantor_acc))
+# pk also works: w3.middleware_onion.add(construct_sign_and_send_raw_middleware(grantor_pk))
+
+# Transactions signed, under the hood, in the middleware:
+# tx1_hash = w3.eth.send_transaction({
+#     "from": grantor_acc.address,
+#     "value": 3333333333,
+#     "to": prover_address
+# })
+
+# tx2_hash = w3.eth.send_transaction({
+#     "from": grantor_acc.address,
+#     "value": w3.to_wei(3, 'ether'),
+#     "to": maintainer_acc.address
+# })
+
+# tx1 = w3.eth.get_transaction(tx1_hash)
+# assert tx1["from"] == grantor_acc.address
+# tx2 = w3.eth.get_transaction(tx2_hash)
+# assert tx2["from"] == grantor_acc.address
+
+
+
